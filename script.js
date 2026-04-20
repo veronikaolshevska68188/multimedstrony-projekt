0f8155cc038ba89983e6008fd8fb1f9093fbdb9f
Oniksiyaa <volshev2@stu.vistula.edu.pl>
2026-04-20 18:53:01 +0300

    zad5_formularz_i_walidacja_68188

--- script.js
+++ script.js
@@ -18,3 +18,62 @@ toggleProjects.addEventListener("click", () => {
         projekty.style.display = "none";
     }
 });
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let isValid = true;
+
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    document.querySelectorAll(".error").forEach(el => el.textContent = "");
+    document.querySelectorAll("input, textarea").forEach(el => el.classList.remove("error-border"));
+
+    function showError(input, msg) {
+        input.classList.add("error-border");
+        input.nextElementSibling.textContent = msg;
+        isValid = false;
+    }
+
+    if (firstName.value.trim() === "") {
+        showError(firstName, "Podaj imię");
+    }
+
+    if (lastName.value.trim() === "") {
+        showError(lastName, "Podaj nazwisko");
+    }
+
+    if (email.value.trim() === "") {
+        showError(email, "Podaj email");
+    }
+
+    if (message.value.trim() === "") {
+        showError(message, "Podaj wiadomość");
+    }
+
+    const nameRegex = /^[A-Za-zÀ-ž\s-]+$/;
+
+    if (firstName.value && !nameRegex.test(firstName.value)) {
+        showError(firstName, "Imię nie może zawierać cyfr");
+    }
+
+    if (lastName.value && !nameRegex.test(lastName.value)) {
+        showError(lastName, "Nazwisko nie może zawierać cyfr");
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email.value && !emailRegex.test(email.value)) {
+        showError(email, "Niepoprawny email");
+    }
+
+    if (isValid) {
+        alert("Udało się");
+        form.reset();
+    }
+});
